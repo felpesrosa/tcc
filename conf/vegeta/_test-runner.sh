630ee334a173ed9e95e6b756c70644b85de12835
Felipe Rosa <feliperosa318@gmail.com>
2023-06-19 17:38:19 -0300

    Resultados e alteraçoes

--- conf/vegeta/_test-runner.sh
+++ conf/vegeta/_test-runner.sh
@@ -13,14 +13,14 @@ read -p "com VirtualThreads, começar? "
 
     sleep 3s
 
-    for iCT1 in $(seq 1 5)
+    for iCT1 in $(seq 1 3)
     do
         curl http://localhost:8080/Servlet/primeNumber?openCSVFile=true
 
         # total de 80_000 requisições em 10s com VirtualThreads
         echo " $iCT1 º loop, VirtualThreads >" $(date)
         echo " $iCT1 º loop, VirtualThreads >" $(date) >> ./runnerlog.txt
-        vegeta attack -duration=10s -rate=8000 -targets=./targets/1_Targers_with_delay.txt | vegeta report > ./results/vt/virtualThreads_loop_$iCT1.txt
+        vegeta attack -duration=10s -rate=5000 -targets=./targets/1_Targers_with_delay.txt | vegeta report > ./results/vt/virtualThreads_loop_$iCT1.txt
         echo "fim $iCT1 º loop com VirtualThreads >" $(date)
         echo "fim $iCT1 º loop com VirtualThreads >" $(date) >> ./runnerlog.txt
         echo ""
@@ -47,14 +47,14 @@ sudo systemctl stop tomcat
 
     sleep 3s
 
-    for iCT2 in $(seq 1 5)
+    for iCT2 in $(seq 1 3)
     do
         curl http://localhost:8080/Servlet/primeNumber?openCSVFile=true
 
         # total de 80_000 requisições em 10s com CachedThreads
         echo " $iCT2 º loop, CachedThreads >" $(date)
         echo " $iCT2 º loop, CachedThreads >" $(date) >> ./runnerlog.txt
-        vegeta attack -duration=10s -rate=8000 -targets=./targets/1_Targers_with_delay.txt | vegeta report > ./results/ct/cachedThreads_loop_$iCT2.txt
+        vegeta attack -duration=10s -rate=5000 -targets=./targets/1_Targers_with_delay.txt | vegeta report > ./results/ct/cachedThreads_loop_$iCT2.txt
         echo "fim $iCT2 º loop com CachedThreads >" $(date)
         echo "fim $iCT2 º loop com CachedThreads >" $(date) >> ./runnerlog.txt
         echo ""
@@ -81,14 +81,14 @@ sudo systemctl stop tomcat
 
     sleep 3s
 
-    for iCT3 in $(seq 1 5)
+    for iCT3 in $(seq 1 3)
     do
         curl http://localhost:8080/Servlet/primeNumber?openCSVFile=true
 
         # total de 80_000 requisições em 10s com PlatformThreads
         echo " $iCT3 º loop, PlatformThreads >" $(date)
         echo " $iCT3 º loop, PlatformThreads >" $(date) >> ./runnerlog.txt
-        vegeta attack -duration=10s -rate=8000 -targets=./targets/1_Targers_with_delay.txt | vegeta report > ./results/pt/PlatformThreads_loop_$iCT3.txt
+        vegeta attack -duration=10s -rate=5000 -targets=./targets/1_Targers_with_delay.txt | vegeta report > ./results/pt/PlatformThreads_loop_$iCT3.txt
         echo "fim $iCT3 º loop com PlatformThreads >" $(date)
         echo "fim $iCT3 º loop com PlatformThreads >" $(date) >> ./runnerlog.txt
         echo ""
@@ -116,14 +116,14 @@ sudo systemctl stop tomcat
 
     sleep 3s
 
-    for iCT4 in $(seq 1 5)
+    for iCT4 in $(seq 1 3)
     do
         curl http://localhost:8080/Servlet/primeNumber?openCSVFile=true
 
         # total de 80_000 requisições em 10s com executor original do Tomcat
         echo " $iCT4 º loop,  executor do Tomcat >" $(date)
         echo " $iCT4 º loop,  executor do Tomcat >" $(date) >> ./runnerlog.txt
-        vegeta attack -duration=10s -rate=8000 -targets=./targets/1_Targers_with_delay.txt | vegeta report > ./results/original/nio_loop_$iCT4.txt
+        vegeta attack -duration=10s -rate=5000 -targets=./targets/1_Targers_with_delay.txt | vegeta report > ./results/original/nio_loop_$iCT4.txt
         echo "fim $iCT4 º loop com  executor do Tomcat >" $(date)
         echo "fim $iCT4 º loop com  executor do Tomcat >" $(date) >> ./runnerlog.txt
         echo ""
